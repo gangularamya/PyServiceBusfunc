535f5da38773f0ba9c1be73afdb3f0bc3492534b
Ramya Gangula <ragangul@microsoft.com>
2024-03-05 09:44:58 -0600

    removed unwanted code

--- Httptrigger2/__init__.py
+++ Httptrigger2/__init__.py
@@ -7,10 +7,7 @@ import time
 
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
-    # service_bus_namespace = "PythonFuncServiceBusResPrem.servicebus.windows.net"
     service_bus_namespace = os.environ.get("ServiceBusConnection__fullyQualifiedNamespace")
-    # connection_string = os.getenv("ServiceBusConnectionString")
-    # queue_name = "demosb"
     queue_name = os.environ.get("ServiceBusQueueName")
     logging.info(f"Service Bus Namespace: {service_bus_namespace}")
     logging.info(f"Queue Name: {queue_name}")
@@ -19,23 +16,8 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
     servicebus_client = ServiceBusClient(
         fully_qualified_namespace=service_bus_namespace,
         credential=credential)
-    # servicebus_client = ServiceBusClient(service_bus_namespace, credential)
     logging.info('Python HTTP trigger function processed a request.')
     
-
-
-    # message = ServiceBusMessage("Hello, Service Bus!")
-
-    # servicebus_client = ServiceBusClient.from_connection_string(connection_string)
-    # with servicebus_client:
-    #     sender = servicebus_client.get_queue_sender(queue_name)
-    #     with sender:
-    #         for i in range(100):
-    #             message = ServiceBusMessage(f"Hello, Service Bus! Message {i+1}")
-    #             sender.send_messages(message)
-                # time.sleep(20)  # pause for 20 seconds
-
-    # sender = servicebus_client.get_queue_sender(queue_name)
     with servicebus_client:
         sender = servicebus_client.get_queue_sender(queue_name)
         with sender:
